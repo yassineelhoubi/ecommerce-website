c7c70d0091e3b2a34ac830a1682c45bfe101f311
yassineelhoubi <elhoubiyassine@gmail.com>
2021-07-27 11:36:47 +0100

    recovery extension html

---
+++ Public/resources/js/includeTemp.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#header").load("../../resources/includes/templates/header.html"); 
+    $("#footer").load("../../resources/includes/templates/footer.html"); 
+    $("#sidebar").load("../../resources/includes/templates/sidebar.html"); 
+  });
